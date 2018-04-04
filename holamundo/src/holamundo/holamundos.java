3f181e0e487d23c17bcc0164109449c66953d663
casereset <casereset@gmail.com>
2018-04-03 22:59:19 -0500

    Mi primer hola Mundo

---
+++ holamundo/src/holamundo/holamundos.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package holamundo;
+
+/**
+ * @author reactor
+ *
+ */
+public class holamundos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Emperador");
+	}
+
+}
